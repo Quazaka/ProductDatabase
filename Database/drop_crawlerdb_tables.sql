f72dde24e0272ca4c558e37f28502282d4d41b65
Software7 <sw7@h57.net.klient.slv.site.aau.dk>
2016-10-20 12:31:10 +0200

    Updated structure, and added sql scheme for crawlerdb

---
+++ Database/drop_crawlerdb_tables.sql
@@ -0,0 +1,3 @@
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE CPU, Chassis, Cons, Cooling, GPU, HardDrive, Motherboard, PSU, Product, Product_Retailer, Pros, RAM, Retailer, Review, ReviewComment, SoundCard;
+SET FOREIGN_KEY_CHECKS = 1;
