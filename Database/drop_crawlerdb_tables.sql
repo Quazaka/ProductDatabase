a91ea50c1ff1c271c2c3c445bfa89f9e24f3bc06
Software7 <sw7@h57.net.klient.slv.site.aau.dk>
2016-11-15 14:46:25 +0100

    Moved files

--- Database/drop_crawlerdb_tables.sql
+++
@@ -1,3 +0,0 @@
-SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE CPU, Chassis, Cons, Cooling, GPU, HardDrive, Motherboard, PSU, Product, Product_Retailer, Pros, RAM, Retailer, Review, ReviewComment, SoundCard;
-SET FOREIGN_KEY_CHECKS = 1;
